6f52fc88d267613de3299f61064b2e3407dbf627
Adam Borowski <fl.borovsky@gmail.com>
2024-01-14 14:45:35 +0100

    feat: add milliionaire

--- src/millionaire.mjs
+++ src/millionaire.mjs
@@ -1,44 +1,31 @@
 const log = (() => {
-  const time = Date.now()
+  const time = Date.now();
   return (message) => {
-    console.log(`${(Date.now() - time).toString().padStart(6, ' ')} ms: ${message}`)
-  }
-})()
+    console.log(
+      `${(Date.now() - time).toString().padStart(6, " ")} ms: ${message}`
+    );
+  };
+})();
 
 const drawAnswer = (challenge) => {
-  const randomIndex = Math.floor(Math.random() *4)
-  return challenge.answers['abcd'[randomIndex]]
-}
+  const randomIndex = Math.floor(Math.random() * 4);
+  return challenge.answers["abcd"[randomIndex]];
+};
 
-const startParticipant = (name, challenge, signal) =>
+const startParticipant = (name, challenge) =>
   new Promise((resolve, reject) => {
     log(`${name} started thinking...`);
 
-    const timeoutId = setTimeout(() => {
+    setTimeout(() => {
       const answer = drawAnswer(challenge);
       log(`${name} is answering ${answer}!`);
-      resolve({name, answer});
+      resolve({ name, answer });
     }, 2000 + Math.random() * 10000); // in total, the person can answer in 2-12 seconds
-
-    signal.addEventListener('abort', () => {
-      log(`${name} is interrupted!`);
-      clearTimeout(timeoutId);
-      reject();
-    })
-
   });
 
 const awaitFirstProperAnswer = (participantSimulators, challenge) => {
-
   return new Promise((resolve, reject) => {
-    participantSimulators.forEach((simulator) => {
-      simulator.then((result) => {
-        if (result.answer === challenge.answer) {
-          resolve(result);
-        }
-      });
-    })
-    Promise.all(participantSimulators).catch(reject)
+    // ?? How to wait for the first proper answer?
   });
 };
 
@@ -67,34 +54,25 @@ export function startMillionaire() {
       },
     };
 
-
-    const abortController = new AbortController();
-
     const stopGame = () => {
-      // ?? How to cancel the game?
-      abortController.abort();
+      // ?? How to cancel all the participants?
       log("Game stopped!");
-      resolve()
+      resolve();
     };
-
 
     const participantSimulators = participants.map((name) =>
       startParticipant(name, challenge, abortController.signal)
     );
 
-
     setTimeout(() => {
-      log("Time is up!");
+      log("Time is up! No one answered correctly!");
       stopGame();
     }, 10000); // after 10 seconds, cancel the game
 
     awaitFirstProperAnswer(participantSimulators, challenge).then((winner) => {
       log(`The winner is ${winner.name} with answer ${winner.answer}`);
-      log('Stopping the game!')
+      log("Stopping the game!");
       stopGame(); // we also cancel the game here, so no one else should work on the answer
-    }).catch(() => {
-      log('No one answered correctly!')
-      stopGame();
-    })
-  })
+    });
+  });
 }
