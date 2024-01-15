ba5aa1aadfe6b2eb8a7a2c3d5260fa12e5a722a8
Adam Borowski <fl.borovsky@gmail.com>
2024-01-15 18:56:32 +0100

    empty solution

--- src/todomgr.mjs
+++ src/todomgr.mjs
@@ -1,91 +1,39 @@
-import { produce, enableMapSet, applyPatches, enablePatches } from "immer";
-
-enablePatches();
 export default function createTodoManager(onPatch) {
   let todos = [];
 
   return {
     add(id, values) {
-      const nextState = produce(
-        todos,
-        (draft) => {
-          draft.push({
-            id,
-            content: values.content,
-            done: false,
-            subnotes: [],
-          });
-        },
-        (patches) => onPatch(patches),
-      );
-
-      todos = nextState;
+      // TODO
+      onPatch("...");
     },
 
     remove(id) {
-      const nextState = produce(
-        todos,
-        (draft) => {
-          const index = draft.findIndex((todo) => todo.id === id);
-          if (index !== -1) draft.splice(index, 1);
-        },
-        (patches) => onPatch(patches),
-      );
-
-      todos = nextState;
+      // TODO
+      onPatch("...");
     },
 
     updateContent(id, content) {
-      const nextState = produce(
-        todos,
-        (draft) => {
-          const todo = draft.find((t) => t.id === id);
-          if (todo) {
-            todo.content = content;
-          }
-        },
-        (patches) => onPatch(patches),
-      );
-
-      todos = nextState;
+      // TODO
+      onPatch("...");
     },
 
     addNote(id, note) {
-      const nextState = produce(
-        todos,
-        (draft) => {
-          const todo = draft.find((t) => t.id === id);
-          if (todo) {
-            todo.subnotes.push(note);
-          }
-        },
-        (patches) => onPatch(patches),
-      );
-
-      todos = nextState;
+      // TODO
+      onPatch("...");
     },
 
     clearNote(id, noteIndex) {
-      const nextState = produce(
-        todos,
-        (draft) => {
-          const todo = draft.find((t) => t.id === id);
-          if (todo && todo.subnotes[noteIndex] !== undefined) {
-            todo.subnotes.splice(noteIndex, 1);
-          }
-        },
-        (patches) => onPatch(patches),
-      );
-
-      todos = nextState;
+      // TODO
+      onPatch("...");
     },
 
     getTodos() {
+      Object.freeze(todos); // ensure we don't give the client the ability to mutate the state
       return todos;
     },
 
     applyPatches(patches) {
-      todos = applyPatches(todos, patches);
+      // TODO
     },
   };
 }
